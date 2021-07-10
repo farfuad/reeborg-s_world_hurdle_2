7a4cf40af887cdd3f2b0a8abf64d34069458dd43
farfuad <86918305+farfuad@users.noreply.github.com>
2021-07-11 01:36:07 +0600

    Add files via upload

---
+++ Hurdle 2.py
@@ -0,0 +1,31 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while at_goal() != True:
+    move()
+    jump()
+    
+
+    
+
+    
+    
+    
+
+    
+        
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
